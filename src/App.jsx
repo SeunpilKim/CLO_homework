416c547ff21523d53708995d467d914cd4930aa4
SeunpilKim <tjsvlf21@naver.com>
2023-08-03 23:56:00 +0900

    그리드 적용, Bootstrap Css 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-import ConditionContainer from './containers/ConditionContainer.js';
+import ConditionContainer from './containers/ConditionContainer.jsx';
 import Container from 'react-bootstrap/Container';
+import Card from './components/Card.jsx'
 import './App.scss'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row } from 'react-bootstrap';
 
 
 
 const App = () => {
+  const [cardData, setCardData] = useState([])
   const getData = async () => {
     try {
       const res = await axios.get('https://closet-recruiting-api.azurewebsites.net/api/data')
-      console.log(res)
-
+      setCardData(res.data)
     } catch (err) {
       console.warn(err)
     }
@@ -23,11 +26,17 @@ const App = () => {
   })
 
   return (
-    <Container fluid className='App'>
+    <Container className='App'>
       <ConditionContainer />
-      {
-        
-      }
+      <Row style={{ width: '100%' }} className="d-flex">
+        {
+          cardData.map(card => {
+            return (
+              <Card item={card}/>
+            )
+          })
+        }
+      </Row>
     </Container>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col'
+import './Card.scss'
 
-const Card = ({ item }) => {
+const PRICING_OPTION = {
+    PAID: 0,
+    FREE: 1,
+    VIEW_ONLY: 2,
+}
+
+const CardComponent = ({ item }) => {
+    const parseItemPrice = () => {
+        if (item.pricingOption === PRICING_OPTION.PAID) {
+            return `$${item.price}`
+        } else if (item.pricingOption === PRICING_OPTION.FREE) return 'FREE'
+        else return 'View Only'
+    }
     return (
-        <Col xs={12} md={6} lg={4} xl={3}>
-            { item.title }
+        <Col sm={6} md={4} xl={3}>
+            <Card style={{ width: '100%', height: '100%' }}>
+                <Card.Img variant="top" src={item.imagePath} style={{ height: '100%' }}/>
+                <Card.Body>
+                    <div className="d-flex align-center">
+                        <div className="">
+                            <Card.Title>{item.title}</Card.Title>
+                            <Card.Text>{item.creator}</Card.Text>
+                        </div>
+                        <div className="ml-auto">
+                            <Card.Text>{parseItemPrice()}</Card.Text>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
         </Col>
     )
 }
 
-export default Card
+export default CardComponent
